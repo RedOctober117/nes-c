f27a9fb045fd1bec5415588b3576036b27083cc7
RedOctober117 <dbcahilly3@gmail.com>
2025-07-13 12:24:56 -0500

    init

--- src/6502.c
+++ src/6502.c
@@ -1,9 +1,23 @@
 #include <stdint.h>
 
+const ZERO_PAGE = 0x0000;
+const STACK = 0x100;
+const RAM = 0x0200;
+const MIRROR_0 = 0x0800;
+const IO_REGISTERS = 0x2000;
+const MIRROR_1 = 0x2008;
+const EXPANSION_ROM = 0x4020;
+const SRAM = 0x6000;
+const PRG_ROM_LOWER = 0x8000;
+const PRG_ROM_UPPER = 0xC000;
+
+const VRAM_WRAP = 0x3FFF;
+
 struct Nes6502
 {
-    uint8_t ram[64000];
     uint16_t program_counter;
+    uint8_t ram[64000];
+    uint8_t vram[16000];
     uint8_t stack_pointer;
     uint8_t accumulator;
     uint8_t register_x;
